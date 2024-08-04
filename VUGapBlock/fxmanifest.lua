1131585a240c69f21e6aaf83f47da7d50e2007f9
Loki <126781899+KeepItLokiTho@users.noreply.github.com>
2024-08-04 16:43:10 -0500

    initial commit

---
+++ VUGapBlock/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resources:
+-- **********
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'KeepItLokiTho'
+description 'Block the gap behind the VU by the highway'
+version '1.0.0'
+
+files {
+	'stream/VUGapBlock.ymap'
+ }
---
+++ jewelry-rats/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resources:
+-- **********
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'KeepItLokiTho'
+description 'Block jewelry gap and jump'
+version '1.0.0'
